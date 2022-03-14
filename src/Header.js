724d6f3ff0cfc94a4d39b34dd31aced31a7bdc4a
Aindrail <aindrailsantra@gmail.com>
2022-03-15 01:23:16 +0530

    Header bar in progress 3 cart and username added

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
 import SearchIcon from '@material-ui/icons/Search';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
 
 const Header = () => {
   return (
@@ -11,19 +13,45 @@ const Header = () => {
             
             </HeaderLogo>
             <HeaderOptionAddress>
-                {/* <LocationOnIcon /> */}
-                {/* <HeaderOption> */}
+                <LocationOnIcon />
+                <HeaderOption>
                     <OptionLineOne>Hello</OptionLineOne>
                     <OptionLineTwo>Select Your Address</OptionLineTwo>
-                {/* </HeaderOption> */}
+                </HeaderOption>
             </HeaderOptionAddress>
             <HeaderSearch>
+
                 <HeaderSearchInput type='text' />
 
                 <HeaderSearchIconContainer>
                     <SearchIcon />
                 </HeaderSearchIconContainer>
+                
             </HeaderSearch>
+            <HeaderNavItems>
+
+                <HeaderOption>
+                      <OptionLineOne>Hello, Aindrail</OptionLineOne>
+                      <OptionLineTwo>Account & Lists</OptionLineTwo>
+                </HeaderOption>
+
+                <HeaderOption>
+                    <OptionLineOne>Return</OptionLineOne>
+                     <OptionLineTwo>/Orders</OptionLineTwo>
+                </HeaderOption>
+
+                <HeaderOptionCart>
+                    <CartCount>
+                        5
+                    </CartCount>
+                    
+                    <ShoppingCartIcon/>  
+                    
+                    
+                </HeaderOptionCart>
+                
+
+            </HeaderNavItems>
     </Container>
   )
 }
@@ -45,20 +73,66 @@ const HeaderLogo = styled.div`
     }
 `
 const HeaderOptionAddress = styled.div`
-
+padding-left: 9px;
+display: flex;
+align-items: center;
 `
 const OptionLineOne = styled.div`
 
 `
 const OptionLineTwo = styled.div`
-
+ font-weight : 700;
 `
 const HeaderSearch = styled.div`
+display: flex;
+flex-grow: 0.8;
+height: 40px;
+border-radius: 5px; 
+overflow: hidden;
+margin-left: 4px;
+:focus-within {
+
+    box-shadow: 0 0 0 3px #F90;
+
+}
 
 `
 const HeaderSearchInput = styled.input`
+flex-grow: 1;
+
+:focus {
+    outline: none;
+} 
+//on hover outline ko hatane ke lea
+
 
 `
 const HeaderSearchIconContainer = styled.div`
+background-color: #febd69;
+width: 45px;
+color: black;
+display: flex;
+justify-content: center;
+align-items: center;
+`
+const HeaderNavItems = styled.div`
+ display: flex;
 
 `
+
+const HeaderOption = styled.div`
+padding: 10px 9px 10px 9px;
+`
+
+const HeaderOptionCart = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+padding-right: 9px;
+`
+
+
+const CartCount = styled.div`
+padding-left: 4px;
+`
+
