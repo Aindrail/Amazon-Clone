2aee49db241d6cf117bead1a76249e308521c333
Aindrail <aindrailsantra@gmail.com>
2022-03-16 11:27:27 +0530

    Cart and Home js file build

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import Header from './Header';
 import './App.css';
+import Cart from './Cart';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Cart />
+      <Home />
     </div>
   );
 }
