fa2579671b938150dbb57c9d557ef061fe497b17
Aindrail <aindrailsantra@gmail.com>
2022-03-17 19:00:59 +0530

    Home page nearly completed

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import Product from './Product'
 
 function Home() {
   return (
@@ -11,8 +12,11 @@ function Home() {
        </Banner>
 
        <Content>
-
+         <Product />  
+         <Product /> 
+         
        </Content>
+       
 
     </Container>
   )
@@ -21,16 +25,22 @@ function Home() {
 export default Home
 
 const Container = styled.div`
-
+    max-width: 100%;
+    margin: 0 auto; // center the cointainer 
 `
 const Banner = styled.div`
-background-image: url('https://i.imgur.com/SYHeuYM.jpg');
-    min-height: 600px;
-    background-position: center;
-     background-size: cover;
+     background-image: url('https://i.imgur.com/SYHeuYM.jpg');
+     min-height: 700px;
+      background-position: center;
+      background-size: cover;
+      z-index: 1;
      mask-image: linear-gradient(to bottom, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0));
 `
 
 const Content = styled.div`
-
+    padding-left: 10px;
+    padding-right: 10px;
+    margin-top: -350px;
+    // z-index:100;
+    display:flex;
 `
